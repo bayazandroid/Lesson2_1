f9c7b2578fc244d1be7683a48b2f4a37cf8713ec
bayazandroid <bayaz.orozobekov@gmail.com>
2022-05-01 18:43:36 +0300

    исправлено

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,40 +1,38 @@
 package com.company;
 
 import java.util.Random;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println(1);
-        nameTemp(23,generateRandom());
+        System.out.println( nameTemp(23, generateRandom()));
         System.out.println(2);
-        nameTemp(10,generateRandom());
+        System.out.println( nameTemp(10, generateRandom()));
         System.out.println(3);
-        nameTemp(18,generateRandom());
+        System.out.println(nameTemp(18, generateRandom()));
         System.out.println(4);
-        nameTemp(20,generateRandom());
+        System.out.println(nameTemp(20, generateRandom()));
         System.out.println(5);
-        nameTemp(38,generateRandom());
+        System.out.println(nameTemp(38, generateRandom()));
     }
     public static String nameTemp(int age, int temperature) {
         if (temperature > 20 && temperature < 40 && age > 20 && age < 45) {
-            System.out.println("можно идти гулять ");
+            return " можно идти гулять ";
+        } else if (age < 20 || temperature > 0 && temperature < 28) {
+            return " можно идти гулять ";
+        } else if (age > 45 || temperature > -10 && temperature < 25) {
+            return " можно идти гулять ";
+        } else {
+            return " оставайся дома ";
         }
-        else if (age < 20 || temperature > 0 && temperature < 28) {
-            System.out.println("можно идти гулять ");
-        }
-        else if (age>45|| temperature>-10 && temperature<25) {
-            System.out.println("можно идти гулять ");
-        } else  {
-            System.out.println("оставаайтесь дома ");
-        }
-    return String.valueOf(age+temperature);
     }
-    public static  int generateRandom(){
+
+    public static int generateRandom() {
         Random random = new Random();
-        int randomNumber = random.nextInt(50)+1;
-        System.out.println(randomNumber);
+        int randomNumber = random.nextInt(50) + 1;
         return randomNumber;
     }
+
 }
